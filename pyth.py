944e09f33d3657d10d77aa5c09ec0c70d801d964
benninwa <53003077+benninwa@users.noreply.github.com>
2025-03-10 17:31:42 +0100

    Python assignment 1

---
+++ pyth.py
@@ -0,0 +1,16 @@
+#my_list = ['wood', 'chair', 'orange', 'apple']
+
+#my_2_list = ['grass', 'machine', 'car']
+
+#a = my_list + my_2_list
+
+#print(a)
+
+my_dict = {
+            "name":"Sam",
+            "age":32,
+            "gender":"male"
+          }
+
+for x in my_dict:
+    print(x)
